1daf93e4ad95316953ccace142fe3109a43fbf71
MoNireu <monireu.dev@gmail.com>
2021-07-15 17:11:04 +0900

    feat: Add SceneCoordinator

---
+++ RxChat/Scene/SceneCoordinator.swift
@@ -0,0 +1,67 @@
+//
+//  SceneCoordinator.swift
+//  RxChat
+//
+//  Created by MoNireu on 2021/07/15.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+
+extension UIViewController {
+    var sceneViewController: UIViewController {
+        return self.children.first ?? self
+    }
+}
+
+class SceneCoordinator: SceneCoordinatorType {
+    private let bag = DisposeBag()
+    
+    private var window: UIWindow
+    private var currentVC: UIViewController
+    
+    required init(window: UIWindow) {
+        self.window = window
+        currentVC = window.rootViewController!
+    }
+    
+    func transition(to scene: Scene, using style: TransitionStyle, animated: Bool) -> Completable {
+        let subject = PublishSubject<Void>()
+        
+        let target = scene.instantiate()
+        
+        switch style {
+        case .fullScreen:
+            target.modalPresentationStyle = .fullScreen
+            currentVC.present(target, animated: true) {
+                subject.onCompleted()
+            }
+            
+            currentVC = target.sceneViewController
+        case .push:
+            <#code#>
+        case .modal:
+            <#code#>
+        }
+    }
+    
+    @discardableResult
+    func close(animated: Bool) -> Completable {
+        return Completable.create { [unowned self] completable in
+            if let presentingVC = self.currentVC.presentingViewController {
+                self.currentVC.dismiss(animated: animated) {
+                    self.currentVC = presentingVC.sceneViewController
+                    completable(.completed)
+                }
+            }
+            else {
+                completable(.error(TransitionError.unknown))
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
+
---
+++ RxChat/Scene/SceneCoordinatorType.swift
@@ -0,0 +1,17 @@
+//
+//  SceneCoordinatorType.swift
+//  RxChat
+//
+//  Created by MoNireu on 2021/07/15.
+//
+
+import Foundation
+import RxSwift
+
+protocol SceneCoordinatorType {
+    @discardableResult
+    func transition(to scene: Scene, using style: TransitionStyle, animated: Bool) -> Completable
+    
+    @discardableResult
+    func close(animated: Bool) -> Completable
+}
