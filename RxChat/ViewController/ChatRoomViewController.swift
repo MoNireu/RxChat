0325a52d0ea1e62b8ad8f43dc569f3af0a1ba7bd
MoNireu <monireu.dev@gmail.com>
2021-12-15 15:42:20 +0900

    feat: Add send chat button

--- RxChat/ViewController/ChatRoomViewController.swift
+++ RxChat/ViewController/ChatRoomViewController.swift
@@ -15,16 +15,25 @@ class ChatRoomViewController: UIViewController, ViewModelBindableType {
     var viewModel: ChatRoomViewModel!
     
     let MAX_LINE: Int = 5
-    let BOTTOM_BAR_DEFAULT_HEIGHT: CGFloat = 80
+    let BOTTOM_BAR_DEFAULT_HEIGHT: CGFloat = 84
+    let CORNER_RADIUS_VALUE: Float = 0.02
+
     
     @IBOutlet weak var contextTextView: UITextView!
     @IBOutlet weak var bottomBarHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sendChatBtn: UIButton!
     
-
+// MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.sceneCoordinator.getCurrentVC().tabBarController?.tabBar.isHidden = true
         self.navigationItem.largeTitleDisplayMode = .never
+        initContextTextView()
+        initSendChatBtn()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        addAdditionalHeightonBottomBar(contextLine: 1)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -32,10 +41,20 @@ class ChatRoomViewController: UIViewController, ViewModelBindableType {
         viewModel.sceneCoordinator.closed()
     }
     
-    private func addAdditionalHeightonBottomBar() {
+// MARK: - UI Init Methods
+    private func initSendChatBtn() {
+        sendChatBtn.setBackgroundColor(.gray, for: .disabled)
+    }
+    
+    private func initContextTextView() {
+        contextTextView.setCornerRadius(value: CORNER_RADIUS_VALUE)
+        contextTextView.layer.borderColor = UIColor.systemGray4.cgColor
+        contextTextView.layer.borderWidth = CGFloat(0.5)
+    }
+    
+    private func addAdditionalHeightonBottomBar(contextLine: Int) {
         guard let font = self.contextTextView.font else { return }
         let contextLine: Int = {
-            let contextLine = self.contextTextView.getLine()
             if contextLine >= MAX_LINE { return MAX_LINE }
             return contextLine
         }()
@@ -51,8 +70,14 @@ class ChatRoomViewController: UIViewController, ViewModelBindableType {
         
         contextTextView.rx.text
             .subscribe(onNext: { text in
-                self.addAdditionalHeightonBottomBar()
+                self.sendChatBtn.isEnabled = (text != "")
+                
+                self.contextTextView.isScrollEnabled = true
+                let line = self.contextTextView.getLine()
+                self.addAdditionalHeightonBottomBar(contextLine: line)
+                self.contextTextView.isScrollEnabled = (line != 1)
             }).disposed(by: rx.disposeBag)
+        
     }
 
 }
@@ -62,5 +87,10 @@ extension UITextView {
     func getLine() -> Int {
         guard let font = self.font else { return 1 }
         return Int(round(self.contentSize.height / font.lineHeight)-1)
+    }
+        
+    func setCornerRadius(value: Float) {
+        self.layer.cornerRadius = self.frame.size.width * CGFloat(value)
+        self.clipsToBounds = true
     }
 }
