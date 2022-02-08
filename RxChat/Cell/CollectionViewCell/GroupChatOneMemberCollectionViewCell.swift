3d156626494724f3a9c231ced95bd571c1593782
MoNireu <monireu.dev@gmail.com>
2022-02-08 18:30:21 +0900

    feat: Add group chat images to chat summary

---
+++ RxChat/Cell/CollectionViewCell/GroupChatOneMemberCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  GroupChatCollectionViewCell.swift
+//  RxChat
+//
+//  Created by MoNireu on 2022/02/08.
+//
+
+import UIKit
+
+class GroupChatOneMemberCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var groupChatImageView1: UIImageView!
+}
+
+class GroupChatTwoMemberCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var groupChatImageView1: UIImageView!
+    @IBOutlet weak var groupChatImageView2: UIImageView!
+}
+
+class GroupChatThreeMemberCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var groupChatImageView1: UIImageView!
+    @IBOutlet weak var groupChatImageView2: UIImageView!
+    @IBOutlet weak var groupChatImageView3: UIImageView!
+}
+
+class GroupChatFourMemberCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var groupChatImageView1: UIImageView!
+    @IBOutlet weak var groupChatImageView2: UIImageView!
+    @IBOutlet weak var groupChatImageView3: UIImageView!
+    @IBOutlet weak var groupChatImageView4: UIImageView!
+}
